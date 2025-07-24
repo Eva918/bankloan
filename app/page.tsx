72264167462fdc358c43a31cbbfd679a9d78a474
Evangeline Suciadi <141349233+Eva918@users.noreply.github.com>
2025-07-24 11:03:36 +0700

    update link

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-3 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[16px] row-start-2 items-center sm:items-start">
        
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            this is a demo test of power bi iframe embedding{" "}
+            this is a demo test of powerbi iframe embedding{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               app/page.tsx
             </code>
@@ -15,21 +15,17 @@ export default function Home() {
           </li>
          
         </ol>
-
-        {/* Power BI iframe embed */}
+  
         <div className="w-full flex justify-center sm:justify-start">
           <iframe
             title="Financial Dashboard"
             width="900"
             height="600"
-            src="https://app.powerbi.com/view?r=eyJrIjoiYjNkYjMyNTEtM2FjMi00NmY5LThkZWEtOGIxMWM2YzdjMWRjIiwidCI6ImJmMzdmMzE1LWJhYzItNDRhOC1hZTZjLTJmMjEzNTFkMjY0YyIsImMiOjEwfQ%3D%3D"
+            src="https://app.powerbi.com/view?r=eyJrIjoiZmQwZDdjZGQtZjg1NS00ZWMzLWE4YmQtZDUwNTFkZjYzNDhhIiwidCI6ImJmMzdmMzE1LWJhYzItNDRhOC1hZTZjLTJmMjEzNTFkMjY0YyIsImMiOjEwfQ%3D%3D"
             frameBorder="0"
             allowFullScreen={true}
           ></iframe>
-        </div>
-
-       
-          
+        </div> 
        
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
